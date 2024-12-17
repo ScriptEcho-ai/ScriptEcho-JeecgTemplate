ae8818b5bba0ebef56c8f2539de463fbb1b3b1c1
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-12-17 18:15:12 +0800

    fix: 实现本地截图功能

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,16 @@
   import { AppProvider } from '/@/components/Application';
   import { useTitle } from '/@/hooks/web/useTitle';
   import { useLocale } from '/@/locales/useLocale';
+  import { onMounted } from 'vue';
 
   // support Multi-language
   const { getAntdLocale } = useLocale();
 
   useTitle();
+
+  onMounted(() => {
+    setTimeout(() => {
+      window.parent?.postMessage({ action: 'on-code-ok' }, '*');
+    }, 5000);
+  });
 </script>
